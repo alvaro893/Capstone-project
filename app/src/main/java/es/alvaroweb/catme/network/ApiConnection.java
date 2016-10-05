66e90eaac79e1bc5125f0ff680854c2be8e833f6
alvaro893 <alvarobolro@gmail.com>
2016-10-05 15:29:23 +0300

    fix: network, now apiconnection is a singleton

--- app/src/main/java/es/alvaroweb/catme/network/ApiConnection.java
+++ app/src/main/java/es/alvaroweb/catme/network/ApiConnection.java
@@ -3,34 +3,28 @@
  */
 package es.alvaroweb.catme.network;
 
-import android.content.Context;
 import android.provider.Settings;
-import android.util.Log;
 
 import es.alvaroweb.catme.BuildConfig;
 import es.alvaroweb.catme.model.Categories;
-import retrofit2.Call;
+import es.alvaroweb.catme.model.GeneralResponse;
+import es.alvaroweb.catme.model.ResponseImages;
+import es.alvaroweb.catme.model.VoteResponse;
 import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
-/*
- * TODO: Create JavaDoc
+/**
+ * Singleton which holds the access to the Api Routes
  */
 public class ApiConnection {
     private static final String URL_BASE = "http://thecatapi.com/";
     private static final String API_KEY = BuildConfig.CAT_API_KEY;
     private static final String USER_ID = Settings.Secure.ANDROID_ID;
-    private Context mContext;
+    private static final ApiConnection INSTANCE = new ApiConnection();
     private ApiRoutes mApiRoutes;
 
-    public ApiConnection(Context mContext) {
-        this.mContext = mContext;
-        setRetrofit();
-    }
-
-    public ApiConnection() {
+    private ApiConnection() {
         setRetrofit();
     }
 
@@ -42,9 +36,23 @@ public class ApiConnection {
         mApiRoutes = mRetrofit.create(ApiRoutes.class);
     }
 
-    public void getCategories(Callback<Categories> callback) {
-        mApiRoutes.getCategories()
-                .enqueue(callback);
+    public static ApiConnection getInstance(){
+        return INSTANCE;
+    }
 
+    public void fetchCategories(Callback<Categories> callback) {
+        mApiRoutes.getCategories().enqueue(callback);
+    }
+
+    public void fetchImagesVotedData(Callback<ResponseImages> callback) {
+        mApiRoutes.getVotedImages(API_KEY, USER_ID).enqueue(callback);
+    }
+
+    public void fetchImagesFavoritedData(Callback<ResponseImages> callback) {
+        mApiRoutes.getFavoriteImages(API_KEY, USER_ID).enqueue(callback);
+    }
+
+    public void sendVotedImage(Callback<VoteResponse> callback, String imageId, int score) {
+        mApiRoutes.sendVoteImage(API_KEY, imageId, USER_ID, score).enqueue(callback);
     }
 }
--- app/src/main/java/es/alvaroweb/catme/network/ApiRoutes.java
+++ app/src/main/java/es/alvaroweb/catme/network/ApiRoutes.java
@@ -3,9 +3,12 @@ package es.alvaroweb.catme.network;
 import es.alvaroweb.catme.model.GeneralResponse;
 import es.alvaroweb.catme.model.ResponseImages;
 import es.alvaroweb.catme.model.Categories;
+import es.alvaroweb.catme.model.VoteResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 /**
  * Copyright (C) 2016 Alvaro Bolanos Rodriguez
@@ -19,11 +22,14 @@ public interface ApiRoutes {
 
     @GET("api/images/getvotes")
     Call<ResponseImages> getVotedImages(@Query("api_key") String apiKey,
-                                          @Query("sub_id") String subId);
+                                        @Query("sub_id") String subId);
 
     @GET("api/images/getfavourite")
     Call<ResponseImages> getFavoriteImages(@Query("api_key") String apiKey, @Query("sub_id") String subId);
 
+    @GET("api/images/vote")
+    Call<VoteResponse> sendVoteImage(@Query("api_key") String apiKey, @Query("image_id") String imageId,
+                                     @Query("sub_id") String subId, @Query("score") int score);
     @GET("/api/images/favourite")
     Call<GeneralResponse> setFavoriteImage(@Query("api_key") String apiKey, @Query("sub_id") String subId,
                                            @Query("action") String action, @Query("image_id") String imageId);
